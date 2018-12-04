1c7b9abd46619237f8bec621eb1173b8cb53c1f6
John Jay Balilo <johnjay.balilo@gmail.com>
2018-12-04 11:07:02 +0800

    what can jquery do

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+// $('h1').css('color','red')
+
+// $("h1").text("Coding Bootcamp")
+
+// $("ul").html("<li>Iron Man</li> <li>Thor</li>")
+
+// $("input").attr("type", "text")
+
+// $("input").val();
+
+
+$("#hide").click( () => {
+	$(".container").addClass("hidden")
+})
+
+$("#show").click( () => {
+	$(".container").removeClass("hidden")
+})
+
+$("#toggle").click( () => {
+	$(".container").toggleClass("hidden")
+})
+
+$("#changeHTML").click( () => {
+	$("#text").html("jQuery")
+})
+
+// $("#changeHeight").click( () => {
+// 	$("img").css("height", 100)
+// })
+
+$("#changeHeight").click( () => {
+	$("img").toggleClass("smaller")
+})
+
+// $("#changeImage").click( () => {
+// 	$("img").attr("src", "assets/images/o.jpg")
+// })
+
+$("#changeImage").click( () => {
+	$("img").toggleClass("o")
+})
+
+// $("#changeStyle").toggle( () => {
+// 	$("#style").css("color", "blue")
+// })
+
+$("#changeStyle").click( () => {
+	$("#style").toggleClass("blue")
+})
